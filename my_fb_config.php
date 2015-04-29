dcd822ed6215f650e077756be610f167f512ce66
crocodile <daniel.vidonyi@googlemail.com>
2015-04-30 00:50:43 +0200

    Update my_fb_config.php

--- my_fb_config.php
+++ my_fb_config.php
@@ -1,6 +1,6 @@
 <?php
-$my_app_id='1554672364787321';
-$my_app_secret='e9c6df1b9d123b0549f99ef6edb59b0c';
+$my_app_id='xxxxxxxxxxxxxxxxxxxxxxxxx';
+$my_app_secret='xxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
 
 // The root url. Make sure you register this on Facebook
 $my_base_url = 'http://localhost:63343/phplogindemo/';
