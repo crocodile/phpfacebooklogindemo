174d74d54348e06a72f63e96bf94ce74030a969c
Dan <daniel.vidonyi@googlemail.com>
2015-04-29 15:09:41 +0200

    A working simple login demo with a single Facebook API call

--- my_config_file.php
+++ my_config_file.php
@@ -1,4 +1,5 @@
 <?php
 
 // The root url. Make sure you register this on Facebook
-$my_base_url = 'http://localhost:63343/phplogindemo/';
+//$my_base_url = 'http://localhost:63343/phplogindemo/';
+$my_base_url = 'http://ec2-54-152-87-246.compute-1.amazonaws.com/phplogindemo/';
